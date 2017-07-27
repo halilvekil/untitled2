1786e9302a469e5d95e21f2fa5703a3d73ec47f7
halilvekil <guaranafoo@gmail.com>
2017-07-27 17:53:13 +0300

    test all

---
+++ spam.py
@@ -0,0 +1,19 @@
+spam = "7"
+spam = spam + "0"
+eggs = int(spam) + 3
+print(float(eggs))
+
+x = 4
+x+=5
+print x
+
+for i in range(10):
+    print(i)
+    i+=1
+
+list = [1, 1, 2, 3, 5, 8, 13]
+print(list[0])
+
+
+for var in list:
+    print(var)
